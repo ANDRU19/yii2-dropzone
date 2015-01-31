eee59fcd7ae4034c4e9b0dc4a6cf9bf4c46d9c9a
ANDRU19 <andriy.ry@gmail.com>
2015-01-31 16:47:29 +0200

    Update DropZoneAsset.php

--- DropZoneAsset.php
+++ DropZoneAsset.php
@@ -3,7 +3,7 @@ namespace andru\dropzone;
 use yii\web\AssetBundle;
 class DropZoneAsset extends AssetBundle
 {
-    public $sourcePath = '@bower/dropzone/dist/';
+    public $sourcePath = '@bower/dropzone/dist';
     public $css = [
         'dropzone.css',
     ];
