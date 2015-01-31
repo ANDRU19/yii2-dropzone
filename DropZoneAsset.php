fa127e591d464827b0db0da3d9f75d40f2311116
Andrew <andriy.ry@gmail.com>
2015-01-31 16:24:30 +0200

    Initial commit with contributors

---
+++ DropZoneAsset.php
@@ -0,0 +1,14 @@
+<?php
+namespace andru\dropzone;
+use yii\web\AssetBundle;
+class DropZoneAsset extends AssetBundle
+{
+    public $sourcePath = '@bower/dropzone/dist/';
+    public $css = [
+        'dropzone.css',
+    ];
+
+    public $js = [
+        'min/dropzone.min.js',
+    ];
+}
